a39f3903355a42cb9a7764660bb87146f5e7ffab
Hristo Vladev <hr.vladev@gmail.com>
2014-02-04 23:22:09 +0200

    [Emacs] Set up keymaps for Ace Jump Mode

--- .emacs.d/setup/setup-ace-jump-mode.el
+++ .emacs.d/setup/setup-ace-jump-mode.el
@@ -1,3 +1,9 @@
-(use-package ace-jump-mode)
+(use-package ace-jump-mode
+  :config
+  (progn
+    ;; Keymaps
+    (define-key evil-motion-state-map "f" 'ace-jump-char-mode)
+    (define-key evil-motion-state-map "F" 'ace-jump-word-mode)
+    (define-key evil-motion-state-map "," 'ace-jump-line-mode)))
 
 (provide 'setup-ace-jump-mode)
