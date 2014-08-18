dbfb6258a50093c84c705b1dc61237049dd6a0b8
Hristo Vladev <hr.vladev@gmail.com>
2014-08-18 14:48:41 +0300

    Add emacsclient aliases

--- zsh/setup/aliases.zsh
+++ zsh/setup/aliases.zsh
@@ -30,3 +30,7 @@ alias gcm='git commit -m'
 alias gp='git push'
 alias glog='git log --oneline --decorate --color --graph'
 alias glg="git log --graph --pretty=format:'%Cred%h%Creset -%C(bold yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+# Emacs
+alias ec='emacsclient -c'
+alias et='emacsclient -t'
