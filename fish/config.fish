c315c0da5ec53a5ecb75fae5a69951527ea99686
Hristo Vladev <hr.vladev@gmail.com>
2019-04-16 11:36:21 +0300

    fish: set LANG

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
+set -x LANG en_GB.utf8
+set -x EDITOR vim
+
 # Git config
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showcolorhints 1
@@ -6,5 +9,3 @@ set -g __fish_git_prompt_char_stateseparator ' '
 # Configure chruby
 source /usr/local/share/chruby/chruby.fish
 source /usr/local/share/chruby/auto.fish
-
-set -x EDITOR vim
